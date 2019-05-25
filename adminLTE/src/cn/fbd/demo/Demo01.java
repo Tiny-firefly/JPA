52d7e76b9a0b6ba0f3dfc224cd1034ba8b85bc02
fanbaodan <963280034@qq.com>
2019-05-26 01:28:27 +0800

    second modify

--- adminLTE/src/cn/fbd/demo/Demo01.java
+++ adminLTE/src/cn/fbd/demo/Demo01.java
@@ -1,7 +1,22 @@
 package cn.fbd.demo;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class Demo01 {
-    public static void first(){
-        System.out.println("chayi");
+    @Test
+    public  void first(){
+        System.out.println("正在测试！！！");
+    }
+    @Before
+    public void init(){
+        System.out.println("测试初始化！");
+    }
+
+    @After
+    public void end(){
+        System.out.println("测试结束，资源释放！");
     }
 }
