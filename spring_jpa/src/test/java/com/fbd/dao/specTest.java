4ce302016230a7b459413593479fd96c1a803693
fanbaodan <963280034@qq.com>
2019-06-09 12:13:54 +0800

    jpa--SPEC

---
+++ spring_jpa/src/test/java/com/fbd/dao/specTest.java
@@ -0,0 +1,138 @@
+package com.fbd.dao;
+
+import com.fbd.domain.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.persistence.criteria.*;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class specTest {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    /**
+     * 根据条件，查询单个对象
+     */
+    @Test
+    public void testSpec() {
+        //匿名内部类
+        /**
+         * 自定义查询条件
+         *      1.实现Specification接口（提供泛型：查询的对象类型）
+         *      2.实现toPredicate方法（构造查询条件）
+         *      3.需要借助方法参数中的两个参数（
+         *          root：获取需要查询的对象属性
+         *          CriteriaBuilder：构造查询条件的，内部封装了很多的查询条件（模糊匹配，精准匹配）
+         *       ）
+         *  案例：根据客户名称查询，查询客户名为泰戈尔的客户
+         *          查询条件
+         *              1.查询方式
+         *                  cb对象
+         *              2.比较的属性名称
+         *                  root对象
+         *
+         */
+        Specification<Customer> spec = new Specification<Customer>() {
+            public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+                // 1.获取比较的属性
+                Path<Object> custName = root.get("custName");
+                //2.构造查询条件  ：    select * from cst_customer where cust_name = '传智播客'
+                /**
+                 * 第一个参数：需要比较的属性（path对象）
+                 * 第二个参数：当前需要比较的取值
+                 */
+                Predicate predicate = cb.equal(custName, "泰戈尔");// 精准匹配
+                return predicate;
+            }
+        };
+        Customer customer = customerDao.findOne(spec);
+        System.out.println(customer);
+
+    }
+
+
+    /**
+     * 多条件查询
+     * 案例：根据客户名（泰戈尔）和客户所属行业查询（it教育）
+     */
+    @Test
+    public void testSpec1() {
+        /**
+         *  root:获取属性
+         *      客户名
+         *      所属行业
+         *  cb：构造查询
+         *      1.构造客户名的精准匹配查询
+         *      2.构造所属行业的精准匹配查询
+         *      3.将以上两个查询联系起来
+         */
+        Specification<Customer> spec = new Specification<Customer>() {
+            public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+                Path<Object> custName = root.get("custName");//客户名称
+                Path<Object> custIndustry = root.get("custIndustry");//所属行业
+
+                //构造查询
+                //1.构造客户名的精准匹配查询
+                Predicate p1 = cb.equal(custName, "泰戈尔");
+                //第一个参数，path（属性），第二个参数，属性的取值
+                //2..构造所属行业的精准匹配查询
+                Predicate p2 = cb.equal(custIndustry, "教育家");
+
+                //3.将多个查询条件组合到一起：组合（满足条件一并且满足条件二：与关系，满足条件一或满足条件二即可：或关系）
+                Predicate and = cb.and(p1, p2);//以与的形式拼接多个查询条件
+                // cb.or();//以或的形式拼接多个查询条件
+                return and;
+            }
+        };
+        Customer customer = customerDao.findOne(spec);
+        System.out.println(customer);
+    }
+
+    /**
+     * 案例：完成根据客户名称的模糊匹配，返回客户列表
+     *      客户名称以 ’泰‘ 开头
+     *
+     * equal ：直接的到path对象（属性），然后进行比较即可
+     * gt，lt,ge,le,like : 得到path对象，根据path指定比较的参数类型，再去进行比较
+     *      指定参数类型：path.as(类型的字节码对象)
+     */
+
+    @Test
+    public void testSpec3() {
+        //构造查询条件
+        Specification<Customer> spec = new Specification<Customer>() {
+            public Predicate toPredicate(Root<Customer> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+                //查询属性：客户名
+                Path<Object> custName = root.get("custName");
+                //查询方式：模糊匹配
+                Predicate like = cb.like(custName.as(String.class), "老%");
+                return like;
+            }
+        };
+        //添加排序
+        //创建排序对象,需要调用构造方法实例化sort对象
+        //第一个参数：排序的顺序（倒序，正序）
+        //   Sort.Direction.DESC:倒序
+        //   Sort.Direction.ASC ： 升序
+        //第二个参数：排序的属性名称
+        Sort sort = new Sort(Sort.Direction.DESC,"custId");
+
+        List<Customer> list = customerDao.findAll(spec,sort);
+        for (Customer customer : list
+                ) {
+            System.out.println(customer);
+        }
+
+    }
+
+
+}
